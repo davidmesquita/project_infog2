4dc8373974f76433ebea275daff538a38a538e1c
davidmesquita <devid.mesquita13@gmail.com>
2021-01-05 20:56:43 -0300

    incremet trader to viewes

--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -11,6 +11,8 @@ from myapp.models import Survivor, Item
 from myapp.serializers import SurvivorSerializer
 from myapp.serializers import Survivor_LocationSerializer, ItemSerializer
 from rest_framework import generics
+from .services.tradeitems import Trader
+from myapp.serializers import SurvivorSerializer
 
 class SurvivorCreate(generics.ListCreateAPIView):
 
@@ -23,3 +25,26 @@ class SurvivorDetail(generics.ListCreateAPIView):
         survivor = get_object_or_404(Survivor, pk=pk)
         serializer = SurvivorSerializer(survivor)
         return Response(serializer.data)
+
+class TradeItems(APIView, Trader):
+
+    def get(self, request, pk_sur_1, pk_sur_2, format=None):
+        survivor_1 = get_object_or_404(Survivor, pk=pk_sur_1)
+        survivor_2 = get_object_or_404(Survivor, pk=pk_sur_2)
+        serializer_1 = SurvivorSerializer(survivor_1)
+        serializer_2 = SurvivorSerializer(survivor_2)
+        data = ((serializer_1.data), (serializer_2.data))
+        return Response(data)
+
+    def patch(self, request, pk_sur_1, pk_sur_2, format=None):
+        survivor_1 = get_object_or_404(Survivor, pk=pk_sur_1)
+        survivor_2 = get_object_or_404(Survivor, pk=pk_sur_2)
+        trader = Trader(request, survivor_1, survivor_2)
+        trade = trader.perform()
+        if trade:
+            serializer_1 = SurvivorSerializer(survivor_1)
+            serializer_2 = SurvivorSerializer(survivor_2)
+            data = ((serializer_1.data), (serializer_2.data))
+            return Response(data)
+        else:
+            raise Exception("Zombie invasion! Permission denied!")
